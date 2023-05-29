caedfd8033c8ec90a99b3f257b01a99392204b2d
Gergely Bod <bodge04@gmail.com>
2023-05-29 15:32:42 +0200

    Update 2fa.blade.php

--- resources/views/2fa.blade.php
+++ resources/views/2fa.blade.php
@@ -21,7 +21,9 @@
                                     <strong>{{ $message }}</strong>
                                 </div>
                             @endif
-
+                            <!-- Ez a kód jelenleg azért van, hogy tesztelni lehessen a létrehozott random felhasználó 2FA azonosítását! -->
+                            Teszt kód: {{$code->code}}
+                            <!-- Itt a vége!-->
                             <label for="code" class="col-md-3 col-form-label text-md-right">Írd be a kódot</label>
                             <input id="code" type="number" class="form-control @error('code') is-invalid @enderror" name="code" value="{{ old('code') }}" required autocomplete="code" autofocus>
                             @error('code')
